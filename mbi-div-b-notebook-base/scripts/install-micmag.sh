c7b5942cff87f57c82f19c8e76caa01dbf05d74a
lrlunin <lunin@campus.tu-berlin.de>
2024-03-26 15:20:22 +0100

    resolved #8

--- mbi-div-b-notebook-base/scripts/install-micmag.sh
+++ mbi-div-b-notebook-base/scripts/install-micmag.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-
+set -e
 cd /opt
 
 git clone --depth 1 https://github.com/MicMag2/MicMag2.git && cd MicMag2/src
 mkdir build && cd build
 
-cmake .. -DPYTHON_LIBRARY=/opt/conda/bin/python -DPYTHON_INCLUDE_DIR=${CONDA_PREFIX}/include/python3.11 -DCMAKE_CXX_FLAGS=-isystem\ /opt/conda/include
+# change the location of python if necessary
+cmake .. -DPYTHON_LIBRARY=/opt/conda/bin/python -DPYTHON_INCLUDE_DIR=/opt/conda/include/python3.11 -DCMAKE_CXX_FLAGS=-isystem\ /opt/conda/include
 cmake --build .
 
+# only CPU version
 cp magneto_cpu.py ../magnum/magneto_cpu.py 2>/dev/null
 cp _magneto_cpu.so ../magnum/_magneto_cpu.so 2>/dev/null
 
-chmod 777 -R /opt/MicMag2
+# allow other users to read and execute
+chmod 755 -R /opt/MicMag2
